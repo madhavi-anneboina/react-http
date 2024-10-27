d64f0414b79bfc9105cdb664b84c82f3eac8a34a
Madhavi Anneboina <madhavi.anneboina@gmail.com>
2024-10-26 20:23:28 -0400

    useEffect added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import ClassCounter from './components/ClassCounter';
 import FunctionCounter from './components/FunctionCounter';
 import HookCounter from './components/HookCounter';
 import FunctionForm from './components/FunctionForm';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
 
 function App() {
   return (
     <div className="App">
-      <FunctionForm />
+      
+       <HookCounterOne />
     </div>
   );
 }
---
+++ src/components/HookCounterOne.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+const HookCounterOne = () => {
+    const [count,setCount] = useState(0)
+    useEffect(()=>{
+        document.title =`You clicked ${count} times`
+    },[])
+  return (
+    <div>
+     <button onClick={()=>setCount(count + 1)}> clicked {count} times</button>
+    </div>
+  )
+}
+
+export default HookCounterOne
